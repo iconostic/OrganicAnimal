d8b1b72f0bd0c95769296e108e9a71e4adce7e0c
iconostic <guddbs4127@naver.com>
2018-05-09 16:53:19 +0900

    commit by hotfix

---
+++ app/src/main/java/com/icono/organicanimal/a_Fragment3_Item2.java
@@ -0,0 +1,64 @@
+package com.icono.organicanimal;
+
+/**
+ * Created by alfo6-10 on 2018-05-04.
+ */
+
+public class a_Fragment3_Item2 {
+    String addr;
+    String htel;
+    String memberNm;
+    String orgNm;
+    String tel;
+
+    public a_Fragment3_Item2() {
+    }
+
+    public a_Fragment3_Item2(String addr, String htel, String memberNm, String orgNm, String tel) {
+        this.addr = addr;
+        this.htel = htel;
+        this.memberNm = memberNm;
+        this.orgNm = orgNm;
+        this.tel = tel;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public String getHtel() {
+        return htel;
+    }
+
+    public String getMemberNm() {
+        return memberNm;
+    }
+
+    public String getOrgNm() {
+        return orgNm;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public void setHtel(String htel) {
+        this.htel = htel;
+    }
+
+    public void setMemberNm(String memberNm) {
+        this.memberNm = memberNm;
+    }
+
+    public void setOrgNm(String orgNm) {
+        this.orgNm = orgNm;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+}
